97dfee69dcd064676e5bd01cd26f4d960676eaa8
Afeez G Lawal <afeezlg@gmail.com>
2024-06-27 16:35:38 +0100

    add view_checkout event

--- src/modules/common/components/cart-totals/index.tsx
+++ src/modules/common/components/cart-totals/index.tsx
@@ -1,10 +1,12 @@
 "use client"
 
+import { removeNullValues } from "@lib/util/my-utils"
 import { formatAmount } from "@lib/util/prices"
 import { InformationCircleSolid } from "@medusajs/icons"
 import { Cart, Order } from "@medusajs/medusa"
 import { Tooltip } from "@medusajs/ui"
-import React from "react"
+import Integraflow from "integraflow-js"
+import React, { useEffect } from "react"
 
 type CartTotalsProps = {
   data: Omit<Cart, "refundable_amount" | "refunded_total"> | Order
@@ -28,9 +30,29 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
     })
   }
 
+  useEffect(() => {
+    console.log({ cart: data })
+    Integraflow.getClient().track(
+      "view_checkout",
+      removeNullValues({
+        shipping_tax_total: data.shipping_tax_total,
+        shipping_total: data.shipping_total,
+        subtotal: data.subtotal,
+        tax_total: data.tax_total,
+        total: data.total,
+        discount_total: data.discount_total,
+        gift_card_total: data.gift_card_total,
+        gift_card_tax_total: data.gift_card_tax_total,
+        item_total: data.items.reduce((total, item) => {
+          return total + item.quantity
+        }, 0),
+      })
+    )
+  }, [])
+
   return (
     <div>
-      <div className="flex flex-col gap-y-2 txt-medium text-ui-fg-subtle ">
+      <div className="flex flex-col gap-y-2 txt-medium text-ui-fg-subtle">
         <div className="flex items-center justify-between">
           <span className="flex gap-x-1 items-center">
             Subtotal
@@ -61,7 +83,7 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
           </span>
         </div>
         <div className="flex justify-between">
-          <span className="flex gap-x-1 items-center ">Taxes</span>
+          <span className="flex gap-x-1 items-center">Taxes</span>
           <span data-testid="cart-taxes" data-value={tax_total || 0}>
             {getAmount(tax_total)}
           </span>
@@ -80,7 +102,7 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
         )}
       </div>
       <div className="h-px w-full border-b border-gray-200 my-4" />
-      <div className="flex items-center justify-between text-ui-fg-base mb-2 txt-medium ">
+      <div className="flex items-center justify-between text-ui-fg-base mb-2 txt-medium">
         <span>Total</span>
         <span
           className="txt-xlarge-plus"
